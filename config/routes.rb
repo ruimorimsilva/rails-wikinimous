bb2a5f17e81e5d1daebcf76643ad5b4d7ad45e83
rui silva <ruimorimsilva@gmail.com>
2023-02-21 16:41:21 +0000

    finishing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/show'
+  # get 'articles/update'
+  # get 'articles/edit'
+  # get 'articles/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :articles
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do
+  Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words
+  )
+  puts 'articles created'
+end
+rails db:seed
